f7976ace945323f1b0a19e84481505f925e501b2
john katua <johnkatua99@gmail.com>
2022-11-15 12:56:18 +0300

    user can navigate to both login and sign up pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Homepage = () => {
+  const navigate = useNavigate();
   return (
-    <div>Homepage</div>
+    <div>
+      <div onClick={() => navigate('/login')}>
+        Go To Login Page
+      </div>
+      <div onClick={() => navigate('/register')}>
+        Go To Sign Up Page
+      </div>
+    </div>
   )
 }
 
