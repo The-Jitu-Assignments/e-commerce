5f459893aeeef5d877c8fc3962ce9f487f18ee96
john katua <johnkatua99@gmail.com>
2022-11-15 12:53:05 +0300

    added routes to login and sign up page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route  path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default AppRoutes;
+export default AppRoutes; 
