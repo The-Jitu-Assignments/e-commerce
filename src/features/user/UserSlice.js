3283c8391e351e01cd7ed8d34822e04cec9c9dee
john katua <johnkatua99@gmail.com>
2022-11-15 13:38:40 +0300

    created a user slice

--- src/features/user/UserSlice.js
+++ src/features/user/UserSlice.js
@@ -1,1 +1,23 @@
-console.log('user slice');
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null
+  },
+  reducers: {
+    register: (state, action) => {
+      state.user = action.payload
+    },
+    login: (state, action) => {
+      state.user = action.payload
+    },
+    logout: (state) => {
+      state.user = null
+    }
+  }
+});
+
+export const { register, login, logout } = userSlice.actions;
+
+// export default userSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userSlice } from "./features/user/UserSlice";
 
 export const store = configureStore({
   reduucer: {
-    
+    user: userSlice
   }
 })
