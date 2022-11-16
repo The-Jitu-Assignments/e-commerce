cd115f32d863e6ed5e675d216839eaee65e8954b
john katua <johnkatua99@gmail.com>
2022-11-16 05:20:53 +0300

    added a registration page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='login--container'>
+      <div className='login--form'>
+        <h2>
+          Sign up page
+        </h2>
+        <div className='login--data'>
+          <label>Name:</label>
+          <input type="text" placeholder='enter a name' />
+        </div>
+        <div className='login--data'>
+          <label>Email:</label>
+          <input type="email" placeholder='enter an email' />
+        </div>
+        <div className='login--data'>
+          <label>Password:</label>
+          <input  type="password" placeholder='enter your password' />
+        </div>
+        <div className='auth--btn__container'>
+          <button className='login--btn'>
+            Register
+          </button>
+          <span className='auth--link'>
+            Already have an account
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
