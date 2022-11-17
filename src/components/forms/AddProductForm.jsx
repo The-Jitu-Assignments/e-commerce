9690dfbf55d2f6eb410a3881405f9c9a6f0debae
john katua <johnkatua99@gmail.com>
2022-11-17 06:02:39 +0300

    added state to manage product being added

--- src/components/forms/AddProductForm.jsx
+++ src/components/forms/AddProductForm.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './forms.css';
 
 const AddProductForm = ({ onClose }) => {
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    imageUrl: '',
+    description: '',
+    price: '',
+    discountRate: ''
+  })
+
+  const handleChange = () => {
+    console.log('working')
+  }
   return (
     <div className='add--product__form'>
       <div className='product--form__item'>
         <label>Name</label>
-        <input type="text" placeholder="enter product name" />
+        <input type="text" placeholder="enter product name" name='name' value={newProduct.name} onChange={handleChange} />
       </div>
       <div className='product--form__item'>
         <label>Image</label>
