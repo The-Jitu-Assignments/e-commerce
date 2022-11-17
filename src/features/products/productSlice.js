b10e5d6c24d7c5ebab09345bbaaa61daca06438c
john katua <johnkatua99@gmail.com>
2022-11-17 06:24:44 +0300

    added an action to add products to cart

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -1,13 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  products: []
+  products: [],
+  cart: []
 }
 
 export const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
-    
+    addToCart: (state, action) => {
+      state.cart = action.payload
+    }
   }
 })
+
+export const { addToCart } = productSlice.actions;
+
+export default productSlice.reducer;
