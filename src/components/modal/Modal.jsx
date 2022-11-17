4067ac5fae32a4d05563b13731690e4c69fbbdbf
john katua <johnkatua99@gmail.com>
2022-11-17 05:29:51 +0300

    added a modal body

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -10,6 +10,37 @@ const Modal = ({ open, onClose }) => {
           <h2>Create new product</h2>
           <div className="closeBtn" onClick={onClose}>X</div>
         </div>
+        <div className="modal--body">
+          <div className='add--product__form'>
+            <div>
+              <label>Name</label>
+              <input type="text" placeholder="enter product name" />
+            </div>
+            <div>
+              <label>Image</label>
+              <input type="text" placeholder='enter an image url' />
+            </div>
+            <div>
+              <label htmlFor="">Description</label>
+              <textarea type="text" placeholder='enter a short description for the product' />
+            </div>
+            <div>
+              <label htmlFor="">
+                Price
+              </label>
+              <input type="number" placeholder='enter product price' />
+            </div>
+            <div>
+              <label htmlFor="">
+                Discount Rate
+              </label>
+              <input type="number" placeholder='enter the discount rate' />
+            </div>
+            <button type="submit">
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
