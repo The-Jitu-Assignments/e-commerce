7892212f613a46b33aa4a442294a3baa9d4f85d3
john katua <johnkatua99@gmail.com>
2022-11-20 22:25:17 +0300

    user can register to the system

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,13 +4,30 @@ import { useDispatch } from 'react-redux';
 import { register } from '../features/user/UserSlice';
 
 const RegisterPage = () => {
+  const [user, setUser] = React.useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setUser((user) => ({
+      ...user,
+      [e.target.name]: e.target.value 
+    }))
+  }
   const navigate = useNavigate()
   const dispatch = useDispatch();
+  // const handleRegister = () => {
+  //   dispatch(register(true));
+  //   navigate('/')
+  // };
 
-  const handleRegister = () => {
-    dispatch(register(true));
+  const handleSubmit = () => {
+    dispatch(register(user));
     navigate('/')
   };
+
   return (
     <div className='login--container'>
       <div className='login--form'>
@@ -19,18 +36,36 @@ const RegisterPage = () => {
         </h2>
         <div className='login--data'>
           <label>Name:</label>
-          <input type="text" placeholder='enter a name' />
+          <input 
+            type="text" 
+            placeholder='enter a name'
+            name='name'
+            value={user.name}
+            onChange={handleChange} 
+          />
         </div>
         <div className='login--data'>
           <label>Email:</label>
-          <input type="email" placeholder='enter an email' />
+          <input 
+            type="email" 
+            placeholder='enter an email' 
+            name='email'
+            value={user.email}
+            onChange={handleChange}
+          />
         </div>
         <div className='login--data'>
           <label>Password:</label>
-          <input  type="password" placeholder='enter your password' />
+          <input 
+            type="password" 
+            placeholder='enter your password' 
+            name='password'
+            value={user.password}
+            onChange={handleChange}
+          />
         </div>
         <div className='auth--btn__container'>
-          <button className='login--btn' onClick={handleRegister}>
+          <button className='login--btn' onClick={handleSubmit}>
             Register
           </button>
           <span className='auth--link' onClick={() => navigate('/login')}>
