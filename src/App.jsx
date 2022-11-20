a6ad2ab6bd25bfa3f28c7f1d07a1fe294b64a7fd
john katua <johnkatua99@gmail.com>
2022-11-20 15:21:22 +0300

    created a sidebar context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import AppRoutes from './AppRoutes'
 import './App.css';
-import { useSelector } from 'react-redux';
 
 const App = () => {
-  const { user } = useSelector((state) => state.user);
-  console.log(user);
   return (
       <AppRoutes />
   )
