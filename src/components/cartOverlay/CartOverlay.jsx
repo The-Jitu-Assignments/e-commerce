3bff2af1588e5a2ec62321ea5979fca4ae55694c
john katua <johnkatua99@gmail.com>
2022-11-20 23:44:07 +0300

    displaying the total number of products to be ordered

--- src/components/cartOverlay/CartOverlay.jsx
+++ src/components/cartOverlay/CartOverlay.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import CartCard from '../cards/cartCard/CartCard';
-import './cart.css'
+import './cart.css';
+import { useSelector } from 'react-redux';
 
 const CartOverlay = ({ open, onClose }) => {
+  const { cart } = useSelector((state) => state.product)
   if (!open) return;
+
+  const totalProducts = cart.reduce((total, item) => total + item.count, 0);
+
   return (
     <div className='overlay' onClick={onClose}>
       <div className='cart--body' onClick={(e) => e.stopPropagation()}>
@@ -13,6 +18,9 @@ const CartOverlay = ({ open, onClose }) => {
         </div>
         <div className='cart--item__container'>
           <CartCard />
+        </div>
+        <div className='cart--footer'>
+          Total Products to be order is: {totalProducts}
         </div>
       </div>
     </div>
