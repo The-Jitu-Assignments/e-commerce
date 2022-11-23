1bc82985a14219e03bb3e084006b57b33eb14698
john katua <johnkatua99@gmail.com>
2022-11-23 04:04:06 +0300

    added initial state for the cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,1 +1,16 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = 'https://react-grid-dashboard-857a2-default-rtdb.firebaseio.com/cart.json';
+
+const initialState = {
+  cart: []
+}
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+  extraReducers: {}
+});
+
+export default cartSlice.reducer;
